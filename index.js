a78456b5e05607c8858cafbbbcc3e65a57131a37
Amar Hussain <99237742+amarhussain01@users.noreply.github.com>
2023-09-19 20:07:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let userName = "  Bro Code  ";
+let phoneNumber = "07412 914 914";
+
+//console.log(userName.length);
+//console.log(userName.charAt(0));
+//console.log(userName.indexOf("o"))
+//console.log(userName.lastIndexOf("o"));
+//console.log(userName.trim();
+userName = userName.toUpperCase();
+userName = userName.toLowerCase();
+
+phoneNumber = phoneNumber.replaceAll("-", "/")
+
+console.log(phoneNumber);
